a61ac7878b0a41c0dad6118425c7f7e29a0f28f5
chtah <chawakorn_ha@hotmail.com>
2023-11-01 16:18:27 +0700

    update handler folder

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,3 +1,47 @@
+import { RequestHandler } from "express";
 import { IUserHandler } from ".";
+import { ICreateUserDto, IUserDto } from "../dto/user";
+import { IErrorDto } from "../dto/error";
+import { IUserRepository } from "../repositories";
+import { hashPassword } from "../utils/bcrypt";
 
-export default class UserHandler implements IUserHandler {}
+export default class UserHandler implements IUserHandler {
+  private repo: IUserRepository;
+
+  constructor(repo: IUserRepository) {
+    this.repo = repo;
+  }
+
+  public registration: RequestHandler<
+    {},
+    IUserDto | IErrorDto,
+    ICreateUserDto
+  > = async (req, res) => {
+    const { name, username, password: plainPassword } = req.body;
+    // const name = req.body.name
+    // const username = req.body.username
+    // const plainPassword = req.body.password
+    /*SAME AS ABOVE*/
+
+    const {
+      id: registerdId,
+      name: registeredName,
+      registeredAt,
+      username: registeredUsername,
+    } = await this.repo.create({
+      name,
+      username,
+      password: hashPassword(plainPassword),
+    });
+
+    return res
+      .status(201)
+      .json({
+        id: registerdId,
+        name: registeredName,
+        registeredAt: `${registeredAt}`, //registeredAt is Date need to change to string
+        username: registeredUsername,
+      })
+      .end();
+  };
+}
